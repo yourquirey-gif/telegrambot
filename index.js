980e9a0322e7c5c2be3e3ab644b30d1ad00ce6ae
yourquirey-gif <yourquirey@gmail.com>
2026-05-15 17:19:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -480,7 +480,7 @@ bot.action(/buy_srv_(.+)/, async (ctx) => {
     ctx.answerCbQuery("Allocating Number...");
 
     // Yahan 'ar' ka matlab Argentina hai (sasta number)
-    const data = await callVakApi('getNumber', { service: service, country: 'ar' });
+    const data = await callVakApi('getNumber', { service: service, country: 'ao' });
 
     if (data && data.tel) {
         const phoneNumber = data.tel;
